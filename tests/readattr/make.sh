46e0e154d0d4a9a73353b31cdbae0be88cce9fcb
Adriaan Dokter <adriaandokter@gmail.com>
2016-06-02 12:30:55 +0200

    remove tests folder from master repo

--- tests/readattr/make.sh
+++
@@ -1,6 +0,0 @@
-clang -DNDEBUG -fwrapv -O3 -Wall -Wstrict-prototypes \
--I/opt/baltrad/rave/include -I/opt/baltrad/HLHDF/include \
--L/opt/baltrad/rave/lib     -L/opt/baltrad/HLHDF/lib  \
--L/opt/local/lib/proj47/lib -I/opt/local/lib/proj47/include \
-./testattr.c \
--o testattr -lravetoolbox -lhlhdf -lproj
