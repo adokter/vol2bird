56e4716441266207ae960dc97b37947f7e3cd91c
Adriaan Dokter <adriaandokter@gmail.com>
2016-10-17 23:00:58 -0400

    updated Dockerfile

--- lib/constants.h
+++ lib/constants.h
@@ -94,9 +94,9 @@
 // Name of the program, to be stored as task attribute in ODIM
 #define PROGRAM "vol2bird"
 // Version of the program, to be stored as task_version attribute in ODIM
-#define VERSION "0.3.2"
+#define VERSION "0.3.3"
 // Date of latest version of the program
-#define VERSIONDATE "29-Sep-2016"
+#define VERSIONDATE "17-Oct-2016"
 
 //-------------------------------------------------------//
 //  user options defaults (to be set in options.conf)    //
