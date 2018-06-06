420168f7ed2dfc4bcd04d3a7bf8be4237a547a42
adokter <adriaandokter@gmail.com>
2018-06-06 14:16:21 -0400

    bump version

--- lib/constants.h
+++ lib/constants.h
@@ -96,9 +96,9 @@
 // Name of the program, to be stored as task attribute in ODIM
 #define PROGRAM "vol2bird"
 // Version of the program, to be stored as task_version attribute in ODIM
-#define VERSION "0.3.18"
+#define VERSION "0.3.19"
 // Date of latest version of the program
-#define VERSIONDATE "19-Oct-2017"
+#define VERSIONDATE "06-Jun-2018"
 
 //-------------------------------------------------------//
 //  user options defaults (to be set in options.conf)    //
