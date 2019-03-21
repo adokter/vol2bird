d610ca5952d4f9798dd023dc748a525bcf2a4ba0
adokter <adriaandokter@gmail.com>
2019-03-20 21:42:12 -0400

    rsl2odim can now read iris format (renaming of rsl2odim needed in future)

--- src/rsl2odim.c
+++ src/rsl2odim.c
@@ -6,35 +6,175 @@
 
 #include <stdio.h>
 #include <float.h>
+#include <getopt.h>
 #include "rave_io.h"
 #include "polarvolume.h"
 #include "libvol2bird.h"
+#include "constants.h"
+
+void usage(char* programName, int verbose){
+    fprintf(stderr,"usage: %s <RSL polar volume input> <ODIM hdf5 volume output>\n",programName);
+    fprintf(stderr,"usage: %s -i <polar volume or scan> [-i <polar scan> [-i <polar scan>] ...] -o <ODIM hdf5 volume output>\n",programName);
+    fprintf(stderr,"usage: %s --help\n", programName);
+
+    if (verbose){
+        fprintf(stderr,"\n   Expects input formats compatible with RSL, see <http://trmm-fc.gsfc.nasa.gov/trmm_gv/software/rsl>\n");
+        fprintf(stderr,"   or Vaisala Sigmet IRIS format, see <ftp://ftp.sigmet.com/outgoing/manuals/IRIS_Programmers_Manual.pdf>\n");
+        fprintf(stderr,"   Outputs OPERA ODIM hdf5 input format, see <http://www.eumetnet.eu/opera-software>\n");
+    }
+}
 
 int main(int argc, char** argv) {
 //    cfg_t* cfg;
 
     // print default message when no input arguments
     if (argc == 1) {
-        fprintf(stderr,"usage: %s <RSL polar volume input> <ODIM hdf5 volume output>\n",argv[0]);
-        fprintf(stderr,"   expects input formats compatible with RSL, see <http://trmm-fc.gsfc.nasa.gov/trmm_gv/software/rsl>\n");
-        fprintf(stderr,"   outputs OPERA ODIM hdf5 input format, see <http://www.eumetnet.eu/opera-software>\n");
-        return -1;
-    }
-    // check to see if we have the right number of input arguments
-    if (argc != 3) {
-        fprintf(stderr, "Error: two arguments required: RSL volume in, and ODIM volume out\n");
+        usage(argv[0], 0);
         return -1;
     }
     
+    // number of input files specified on command line
+    int nInputFiles = 0;
     // the polar volume file that the user provided as input
-    char* fileVolIn = argv[1];
-    // the volume file that the user specified as output
-    const char* fileVolOut = argv[2];
+    char* fileIn[INPUTFILESMAX];
+    // the (optional) vertical profile file that the user specified as output
+    const char* fileVolOut = NULL;
+
+    // determine whether we deal with legacy command line format (0) or getopt command line format (1)
+    int commandLineFormat = 0;
+    for (int i=0; i<argc; i++){
+        if(strcmp("-i",argv[i])==0 || strcmp("--input",argv[i])==0  || \
+           strcmp("-o",argv[i])==0 || strcmp("--output",argv[i])==0 || \
+           strcmp("-h",argv[i])==0 || strcmp("--help",argv[i])==0   || \
+           strcmp("-v",argv[i])==0 || strcmp("--version",argv[i])==0)
+           {
+            commandLineFormat = 1;
+        }
+    }
+
+    // interpret legacy command line input
+    if (commandLineFormat == 0){
+        // check to see if we have the right number of input arguments
+        if (argc != 3) {
+            fprintf(stderr, "Error: Invalid command line arguments\n");
+            usage(argv[0], 0);
+            return -1;
+        }
         
-    // read in data up to a distance of alldata.misc.rCellMax
-    // we do not read in the full volume for speed/memory
+        // ------------------------------------------------------------- //
+        //                initialization of variables                    //
+        // ------------------------------------------------------------- //
+
+        // the polar volume file that the user provided as input
+        fileIn[0] = argv[1];
+        nInputFiles = 1;
+   
+        // the volume file that the user specified as output
+        fileVolOut = argv[2];
+
+    }
+    
+    else{ // interpret command line input using getopt library
+    
+        int c;
+
+        while (1) {
+            static struct option long_options[] =
+            {
+                  /* These options don’t set a flag.
+                     We distinguish them by their indices. */
+                {"help",    no_argument,       0, 'h'},
+                {"version", no_argument,       0, 'v'},
+                {"input",   required_argument, 0, 'i'},
+                {"output",  required_argument, 0, 'o'},
+                {0, 0, 0, 0}
+            };
+            
+            /* getopt_long stores the option index here. */
+            int option_index = 0;
+    
+            c = getopt_long (argc, argv, "hvi:o:",
+                           long_options, &option_index);
+
+            /* Detect the end of the options. */
+            if (c == -1) break;
+
+            switch (c){
+                case 0:
+                /* If this option set a flag, do nothing else now. */
+                    if (long_options[option_index].flag != 0)
+                        break;
+                    printf ("option %s", long_options[option_index].name);
+                    if (optarg)
+                        printf (" with arg %s", optarg);
+                    printf ("\n");
+                    break;
+
+                case 'h':
+                    usage(argv[0],1);
+                    return -1;
+                    break;
+
+                case 'v':
+                    fprintf(stderr,"%s version %s (%s)\n", argv[0], VERSION, VERSIONDATE);
+                    return -1;
+                    break;
+
+                case 'i':
+                    if (nInputFiles < INPUTFILESMAX){
+                        fileIn[nInputFiles] = optarg;
+                        nInputFiles++;
+                    }
+                    else{
+                        fprintf(stderr, "Warning: too many input files, ignoring file %s ...\n", optarg);
+                    }
+                    break;
+
+                case 'o':
+                    fileVolOut = optarg;
+                    break;
+
+                case '?':
+                    /* getopt_long already printed an error message. */
+                    break;
+
+                default:
+                    abort ();
+            }
+        }
+
+        /* Print any remaining command line arguments (not options). */
+        if (optind < argc) {
+            printf ("unknown function argument(s): ");
+            while (optind < argc)
+                printf ("%s ", argv[optind++]);
+            putchar ('\n');
+        }        
+    }
+
+    // check that we have an output file specified
+    if(fileVolOut == NULL){
+        fprintf(stderr, "Error: no output file specified\n");
+        return -1;
+    }
+
+    // check that we have at least one input file specified
+    if(nInputFiles < 1){
+        fprintf(stderr, "Error: no input file(s) specified\n");
+        return -1;
+    }
+
+    // check that input files exist
+    for (int i=0; i<nInputFiles; i++){
+        if(!isRegularFile(fileIn[i])){
+            fprintf(stderr, "Error: input file '%s' does not exist.\n", fileIn[i]);
+            return -1;
+        }
+    }
+        
+    // read in data for the full range of distances.
     PolarVolume_t* volume = NULL;
-    volume = vol2birdGetVolume(&fileVolIn, 1, 1000000, 0);
+    volume = vol2birdGetVolume(fileIn, nInputFiles, 1000000, 0);
     
     if (volume == NULL) {
         fprintf(stderr,"Error: failed to read radar volume\n");
