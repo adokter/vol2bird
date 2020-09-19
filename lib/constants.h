8389c4087b03a72561a92300b6a9212cafd0d109
adokter <adriaandokter@gmail.com>
2020-09-19 12:37:42 -0400

    bump version to signal RSL dependency update

--- lib/constants.h
+++ lib/constants.h
@@ -126,9 +126,9 @@
 // Name of the program, to be stored as task attribute in ODIM
 #define PROGRAM "vol2bird"
 // Version of the program, to be stored as task_version attribute in ODIM
-#define VERSION "0.5.0.9156"
+#define VERSION "0.5.0.9158"
 // Date of latest version of the program
-#define VERSIONDATE "21-Aug-2020"
+#define VERSIONDATE "19-Sep-2020"
 
 
 //-------------------------------------------------------//
