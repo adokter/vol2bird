050d37d6a2107dc0d128cf9820008aee793c9b06
Adriaan Dokter <adriaandokter@gmail.com>
2023-03-31 10:45:55 -0400

    bump version

--- lib/constants.h
+++ lib/constants.h
@@ -128,7 +128,7 @@
 // Name of the program, to be stored as task attribute in ODIM
 #define PROGRAM "vol2bird"
 // Version of the program, to be stored as task_version attribute in ODIM
-#define VERSION "0.5.0.9199"
+#define VERSION "0.6.0"
 // Date of latest version of the program
 #define VERSIONDATE "31-Mar-2023"
 
