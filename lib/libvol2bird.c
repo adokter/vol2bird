a0efcbd57049c71f0c101d7e087758c0bf530f2c
iskandari <sasha.tedeschi@gmail.com>
2023-04-19 10:02:07 -0400

    added functions to check constraints

--- lib/libvol2bird.c
+++ lib/libvol2bird.c
@@ -3469,8 +3469,9 @@ bool validate_value(const field_t field, const char *value) {
         }
         double d_value = atof(value);
 
-        if (field.constraints.minimum != NULL && d_value < *(double*)field.constraints.minimum)) {  
+        if (field.constraints.minimum != NULL && d_value < *(double*)field.constraints.minimum) {  
             printf("Value for field '%s' is below minimum value of %s: %s\n", fields[i].name, field.constraints.minimum, value);
+            return false;
         }   
         if (field.constraints.maximum != NULL && strcmp(*(double*)field.constraints.maximum, "Inf") != 0 && d_value > *(double*)field.constraints.maximum) {
             printf("Value for field '%s' is above maximum value of %s: %s\n", fields[i].name, field.constraints.maximum, value);
