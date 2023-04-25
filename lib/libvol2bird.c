afbbb85330e783c936d275ab1f1d5ebf7b43566e
iskandari <sasha.tedeschi@gmail.com>
2023-04-25 00:01:34 -0400

    export coordinates in decimal degrees not radians

--- lib/libvol2bird.c
+++ lib/libvol2bird.c
@@ -3693,7 +3693,7 @@ void writeCSV(char *filename, vol2bird_t* alldata, char* source, char* fileIn, c
             }
         }
 
-        validate_fields(fields, 3, vpts_values);
+        validate_fields(fields, 10, vpts_values);
 
         /*
         //write to CSV format
