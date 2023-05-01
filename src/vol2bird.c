db9dc8ddf7b7faf8f30c1c7dd449f776a1815387
iskandari <sasha.tedeschi@gmail.com>
2023-05-01 02:35:03 -0400

    make isCSV case insensitive

--- src/vol2bird.c
+++ src/vol2bird.c
@@ -454,12 +454,12 @@ int main(int argc, char **argv)
 
     //check if file extension is csv
     int isCSV(const char *filename) {
-        const char *dot = strrchr(filename, '.');
-        if (dot && !strcmp(dot, ".csv")) {
-            return 1;
-        }
-        return 0;
+    const char *dot = strrchr(filename, '.');
+    if (dot && !strcasecmp(dot, ".csv")) {
+        return 1;
     }
+    return 0;
+}
 
     // save rave profile to ODIM hdf5 or generate VPTS csv based on existence of .csv extension
     if (fileVpOut != NULL)
